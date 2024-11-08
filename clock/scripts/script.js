51567d722e790f833ffe70ac01fca4b791ad4bd9
DaniilKataev <kataev.danechka@mail.ru>
2024-11-08 21:47:15 +0300

    fix hour arrow

--- clock/scripts/script.js
+++ clock/scripts/script.js
@@ -6,9 +6,9 @@ document.querySelector('#hours').textContent = hours;
 document.querySelector('#minutes').textContent = minutes;
 document.querySelector('#seconds').textContent = seconds;
 
-const startHourDeg = ((hours % 12) / 12) * 360;
 const startSecondsDeg = (seconds / 60) * 360;
-const startMinuteDeg = (minutes / 60) * 360;
+const startMinuteDeg = ((minutes * 60 + seconds) / 3600) * 360;
+const startHourDeg = (((hours * 60 + minutes) * 60 + seconds ) / 86400) * 720;
 
 document.body.style.setProperty('--start-hour-deg', `${startHourDeg}deg`);
 document.body.style.setProperty('--start-minute-deg', `${startMinuteDeg}deg`);
